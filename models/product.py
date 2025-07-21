ddd71cbd369df9fe80ac926d6b70bbc4089efb03
Afonso Mateus <afonsomateus.dev@gmail.com>
2025-07-21 20:08:34 -0300

    feat: cria o modulo models e o model Product

---
+++ models/product.py
@@ -0,0 +1,68 @@
+from datetime import date, datetime
+from uuid import uuid4
+
+class Product:
+  def __init__(
+    self, 
+    id: uuid4,
+    name: str, 
+    description: str, 
+    price: float, 
+    expiration_date: date, 
+    barcode: str,
+    created_at: datetime, 
+    updated_at: datetime,
+  ):
+    self.__id = id
+    self.__name = name
+    self.__description = description
+    self.__price = price
+    self.__expiration_date = expiration_date
+    self.__barcode = barcode
+    self.__created_at = created_at
+    self.__updated_at = updated_at
+
+  def set_name(self, name: str):
+    self.__name = name
+
+  def set_description(self, description: str):
+    self.__description = description
+
+  def set_price(self, price: float):
+    self.__price = price
+
+  def set_expiration_date(self, expiration_date: date):
+    self.__expiration_date = expiration_date
+
+  def set_barcode(self, barcode: str):
+    self.__barcode = barcode
+
+  def set_created_at(self, created_at: datetime):
+    self.__created_at = created_at  
+
+  def set_updated_at(self, updated_at: datetime):
+    self.__updated_at = updated_at
+
+  def get_id(self):
+    return self.__id
+
+  def get_name(self):
+    return self.__name
+
+  def get_description(self):
+    return self.__description
+
+  def get_price(self):
+    return self.__price
+
+  def get_expiration_date(self):
+    return self.__expiration_date
+
+  def get_barcode(self):
+    return self.__barcode
+
+  def get_created_at(self):
+    return self.__created_at
+
+  def get_updated_at(self):
+    return self.__updated_at    
